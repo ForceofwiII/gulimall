7bb0b6f45e05ff44301bfb7df9cda6cfc7027623
Force of will <ywu279225@gmail.com>
2024-07-04 11:01:56 +0800

    1

--- gulimall-order/src/test/java/com/atguigu/gulimall/order/GulimallOrderApplicationTests.java
+++ gulimall-order/src/test/java/com/atguigu/gulimall/order/GulimallOrderApplicationTests.java
@@ -1,13 +1,25 @@
 package com.atguigu.gulimall.order;
 
-import org.junit.jupiter.api.Test;
+import com.atguigu.gulimall.order.entity.OrderEntity;
+import com.atguigu.gulimall.order.service.OrderService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class GulimallOrderApplicationTests {
+public class GulimallOrderApplicationTests {
+
+    @Autowired
+    OrderService orderService;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+
+
+        orderService.removeById(1L);
     }
 
 }
--- gulimall-ware/src/test/java/com/atguigu/gulimall/ware/GulimallWareApplicationTests.java
+++ gulimall-ware/src/test/java/com/atguigu/gulimall/ware/GulimallWareApplicationTests.java
@@ -1,13 +1,27 @@
 package com.atguigu.gulimall.ware;
 
-import org.junit.jupiter.api.Test;
+import com.atguigu.gulimall.ware.entity.PurchaseEntity;
+import com.atguigu.gulimall.ware.service.PurchaseService;
+import com.atguigu.gulimall.ware.service.WareSkuService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class GulimallWareApplicationTests {
+public class GulimallWareApplicationTests {
+
+       @Autowired
+    PurchaseService purchaseService;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+
+
+        purchaseService.removeById(1L);
+
     }
 
 }
