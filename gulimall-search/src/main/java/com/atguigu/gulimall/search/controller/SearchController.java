c5969acfed566498c53026d6bfbc9bacfeb2ba7f
Force of will <ywu279225@gmail.com>
2024-07-27 17:56:26 +0800

    1

---
+++ gulimall-search/src/main/java/com/atguigu/gulimall/search/controller/SearchController.java
@@ -0,0 +1,15 @@
+package com.atguigu.gulimall.search.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SearchController {
+
+
+    @GetMapping("/list.html")
+    public  String listpage(){
+        return "index";
+    }
+}
