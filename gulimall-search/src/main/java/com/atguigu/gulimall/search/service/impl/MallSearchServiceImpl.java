e74ce3235c9be4a112aaeedaefb29e670e790879
Force of will <ywu279225@gmail.com>
2024-07-29 17:16:45 +0800

    1

--- gulimall-search/src/main/java/com/atguigu/gulimall/search/service/impl/MallSearchServiceImpl.java
+++ gulimall-search/src/main/java/com/atguigu/gulimall/search/service/impl/MallSearchServiceImpl.java
@@ -1,11 +1,24 @@
 package com.atguigu.gulimall.search.service.impl;
 
+import com.atguigu.gulimall.search.config.GulimallElasticSearchConfig;
+import com.atguigu.gulimall.search.constant.EsConstant;
 import com.atguigu.gulimall.search.service.MallSearchService;
 import com.atguigu.gulimall.search.vo.SearchParam;
 import com.atguigu.gulimall.search.vo.SearchResult;
+import org.apache.commons.lang.StringUtils;
+import org.apache.lucene.search.join.ScoreMode;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.NestedQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.RangeQueryBuilder;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 
 @Service
@@ -17,10 +30,119 @@ public class MallSearchServiceImpl  implements MallSearchService {
 
 
     @Override
-    public SearchResult search(SearchParam searchParam) {
+    public SearchResult search(SearchParam searchParam) throws IOException {
+        //根据条件分页排序高亮聚合检索
+
+        SearchResult result = null;
+
+        SearchRequest searchRequest = buildSerchRequest(searchParam);
+
+
+        try {
+            SearchResponse response = client.search(searchRequest, GulimallElasticSearchConfig.COMMON_OPTIONS);
+
+            result = buildSearchResult(response);
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
 
 
 
+      return result;
+
+    }
+
+    private SearchRequest buildSerchRequest(SearchParam param){
+
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        /**
+         * 模糊匹配，过滤（按照属性，分类，品牌，价格区间，库存）
+         */
+        //1. 构建bool-query
+        BoolQueryBuilder boolQueryBuilder=new BoolQueryBuilder();
+
+        //1.1 bool-must
+        if(!StringUtils.isEmpty(param.getKeyword())){
+            boolQueryBuilder.must(QueryBuilders.matchQuery("skuTitle",param.getKeyword()));
+        }
+
+        //1.2 bool-fiter
+        //1.2.1 catelogId
+        if(null != param.getCatalog3Id()){
+            boolQueryBuilder.filter(QueryBuilders.termQuery("catalogId",param.getCatalog3Id()));
+        }
+
+        //1.2.2 brandId
+        if(null != param.getBrandId() && param.getBrandId().size() >0){
+            boolQueryBuilder.filter(QueryBuilders.termsQuery("brandId",param.getBrandId()));
+        }
+
+        //1.2.3 attrs
+        if(param.getAttrs() != null && param.getAttrs().size() > 0){
+
+            param.getAttrs().forEach(item -> {
+                //attrs=1_5寸:8寸&2_16G:8G
+                BoolQueryBuilder boolQuery = QueryBuilders.boolQuery();
+
+
+                //attrs=1_5寸:8寸
+                String[] s = item.split("_");
+                String attrId=s[0];
+                String[] attrValues = s[1].split(":");//这个属性检索用的值
+                boolQuery.must(QueryBuilders.termQuery("attrs.attrId",attrId));
+                boolQuery.must(QueryBuilders.termsQuery("attrs.attrValue",attrValues));
+
+                NestedQueryBuilder nestedQueryBuilder = QueryBuilders.nestedQuery("attrs",boolQuery, ScoreMode.None);
+                boolQueryBuilder.filter(nestedQueryBuilder);
+            });
+
+        }
+
+        //1.2.4 hasStock
+        if(null != param.getHasStock()){
+            boolQueryBuilder.filter(QueryBuilders.termQuery("hasStock",param.getHasStock() == 1));
+        }
+
+
+        //1.2.5 skuPrice
+        if(!StringUtils.isEmpty(param.getSkuPrice())){
+            //skuPrice形式为：1_500或_500或500_
+            RangeQueryBuilder rangeQueryBuilder = QueryBuilders.rangeQuery("skuPrice");
+            String[] price = param.getSkuPrice().split("_");
+            if(price.length==2){
+
+                if(param.getSkuPrice().startsWith("_")){
+                    rangeQueryBuilder.lte(price[1]);
+                }
+                else{
+                    rangeQueryBuilder.gte(price[0]).lte(price[1]);
+                }
+            }else if(price.length == 1){
+
+                if(param.getSkuPrice().endsWith("_")){
+                    rangeQueryBuilder.gte(price[0]);
+                }
+            }
+            boolQueryBuilder.filter(rangeQueryBuilder);
+        }
+
+        //封装所有的查询条件
+        sourceBuilder.query(boolQueryBuilder);
+
+
+
+        SearchRequest searchRequest = new SearchRequest( new String[]{EsConstant.PRODUCT_INDEX}, sourceBuilder);
+
+        return searchRequest;
+
+
+    }
+
+    private SearchResult buildSearchResult(SearchResponse  response) {
+        SearchResult result = new SearchResult();
+        return result;
 
 
     }
